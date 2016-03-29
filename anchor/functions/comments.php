9c752320b8c66734fe0d17a3f0bb9313187fccc7
Charlie Callow <wollac11@gmail.com>
2016-03-29 21:03:40 +0100

    Merge changes

--- anchor/functions/comments.php
+++ anchor/functions/comments.php
@@ -4,10 +4,11 @@
 *	Theme functions for comments
 */
 
-function has_comments() {
-	if( ! $itm = Registry::get('article')) {
-		return false;
-	}
+function has_comments()
+{
+    if (! $itm = Registry::get('article')) {
+        return false;
+    }
 
 	if( ! $comments = Registry::get('comments')) {
 		$comments = Comment::where('status', '=', 'approved')->where('post', '=', $itm->id)->where('reply_to', '=', 0)->get();
@@ -19,12 +20,12 @@ function has_comments() {
 			$comments[$key]->data['replies'] = new Items($replies);
 		}
 
-		$comments = new Items($comments);
+        $comments = new Items($comments);
 
-		Registry::set('comments', $comments);
-	}
+        Registry::set('comments', $comments);
+    }
 
-	return $comments->length();
+    return $comments->length();
 }
 
 function has_replies() {
@@ -37,26 +38,29 @@ function has_replies() {
 function total_comments() {
 	if( ! has_comments()) return 0;
 
-	$comments = Registry::get('comments');
+    $comments = Registry::get('comments');
 
-	return $comments->length();
+    return $comments->length();
 }
 
 // loop comments
-function comments() {
-	if( ! has_comments()) return false;
+function comments()
+{
+    if (! has_comments()) {
+        return false;
+    }
 
-	$comments = Registry::get('comments');
+    $comments = Registry::get('comments');
 
-	if($result = $comments->valid()) {
-		// register single comment
-		Registry::set('comment', $comments->current());
+    if ($result = $comments->valid()) {
+        // register single comment
+        Registry::set('comment', $comments->current());
 
-		// move to next
-		$comments->next();
-	}
+        // move to next
+        $comments->next();
+    }
 
-	return $result;
+    return $result;
 }
 
 function replies() {
@@ -76,57 +80,69 @@ function replies() {
 }
 
 // single comments
-function comment_id() {
-	return Registry::prop('comment', 'id');
+function comment_id()
+{
+    return Registry::prop('comment', 'id');
 }
 
-function comment_time() {
-	if($time = Registry::prop('comment', 'date')) {
-		return Date::format($time,'U');
-	}
+function comment_time()
+{
+    if ($time = Registry::prop('comment', 'date')) {
+        return Date::format($time, 'U');
+    }
 }
 
-function comment_date() {
-	if($date = Registry::prop('comment', 'date')) {
-		return Date::format($date);
-	}
+function comment_date()
+{
+    if ($date = Registry::prop('comment', 'date')) {
+        return Date::format($date);
+    }
 }
 
-function comment_name() {
-	return Registry::prop('comment', 'name');
+function comment_name()
+{
+    return Registry::prop('comment', 'name');
 }
 
-function comment_email() {
-	return Registry::prop('comment', 'email');
+function comment_email()
+{
+    return Registry::prop('comment', 'email');
 }
 
-function comment_text() {
-	return Registry::prop('comment', 'text');
+function comment_text()
+{
+    return Registry::prop('comment', 'text');
 }
 
-function comments_open() {
-	return Registry::prop('article', 'comments') ? true : false;
+function comments_open()
+{
+    return Registry::prop('article', 'comments') ? true : false;
 }
 
 // form elements
-function comment_form_notifications() {
-	return Notify::read();
+function comment_form_notifications()
+{
+    return Notify::read();
 }
 
-function comment_form_url() {
-	return Uri::to(Uri::current());
+function comment_form_url()
+{
+    return Uri::to(Uri::current());
 }
 
-function comment_form_input_name($extra = '') {
-	return '<input name="name" id="name" type="text" ' . $extra . ' value="' . Input::previous('name') . '">';
+function comment_form_input_name($extra = '')
+{
+    return '<input name="name" id="name" type="text" ' . $extra . ' value="' . Input::previous('name') . '">';
 }
 
-function comment_form_input_email($extra = '') {
-	return '<input name="email" id="email" type="email" ' . $extra . ' value="' . Input::previous('email') . '">';
+function comment_form_input_email($extra = '')
+{
+    return '<input name="email" id="email" type="email" ' . $extra . ' value="' . Input::previous('email') . '">';
 }
 
-function comment_form_input_text($extra = '') {
-	return '<textarea name="text" id="text" ' . $extra . '>' . Input::previous('text') . '</textarea>';
+function comment_form_input_text($extra = '')
+{
+    return '<textarea name="text" id="text" ' . $extra . '>' . Input::previous('text') . '</textarea>';
 }
 
 function comment_form_button($text = 'Post Comment', $extra = '') {
@@ -163,4 +179,5 @@ function reply_email() {
 
 function reply_text() {
 	return Registry::prop('reply', 'text');
+
 }
--- anchor/models/post.php
+++ anchor/models/post.php
@@ -1,99 +1,115 @@
 <?php
 
-class Post extends Base {
+class post extends Base
+{
 
-	public static $table = 'posts';
+    public static $table = 'posts';
 
-	public static function id($id) {
-		return static::get('id', $id);
-	}
+    public static function id($id)
+    {
+        return static::get('id', $id);
+    }
 
-	public static function slug($slug) {
-		return static::get('slug', $slug);
-	}
+    public static function slug($slug)
+    {
+        $post = static::get('slug', $slug);
+        $post->total_comments = static::getCommentCount($post);
+        return $post;
+    }
 
-	private static function get($row, $val) {
-		return static::left_join(Base::table('users'), Base::table('users.id'), '=', Base::table('posts.author'))
-			->where(Base::table('posts.'.$row), '=', $val)
-			->fetch(array(Base::table('posts.*'),
-				Base::table('users.id as author_id'),
-				Base::table('users.bio as author_bio'),
-				Base::table('users.real_name as author_name')));
-	}
+    private static function getCommentCount($post)
+    {
+        return (int)static::left_join(Base::table('comments'), Base::table('comments.post'), '=', Base::table('posts.id'))
+            ->where(Base::table('posts.id'), '=', $post->id)
+            ->count();
+    }
 
-	public static function listing($category = null, $page = 1, $per_page = 10) {
-		// get total
-		$query = static::left_join(Base::table('users'), Base::table('users.id'), '=', Base::table('posts.author'))
-			->where(Base::table('posts.status'), '=', 'published');
+    private static function get($row, $val)
+    {
+        return static::left_join(Base::table('users'), Base::table('users.id'), '=', Base::table('posts.author'))
+            ->where(Base::table('posts.'.$row), '=', $val)
+            ->fetch(array(Base::table('posts.*'),
+                Base::table('users.id as author_id'),
+                Base::table('users.bio as author_bio'),
+                Base::table('users.email as author_email'),
+                Base::table('users.real_name as author_name')));
+    }
 
-		if($category) {
-			$query->where(Base::table('posts.category'), '=', $category->id);
-		}
+    public static function listing($category = null, $page = 1, $per_page = 10)
+    {
+        // get total
+        $query = static::left_join(Base::table('users'), Base::table('users.id'), '=', Base::table('posts.author'))
+            ->where(Base::table('posts.status'), '=', 'published');
 
-		$total = $query->count();
+        if ($category) {
+            $query->where(Base::table('posts.category'), '=', $category->id);
+        }
 
-		// get posts
-		$posts = $query->sort(Base::table('posts.created'), 'desc')
+        $total = $query->count();
+
+        // get posts
+        $posts = $query->sort(Base::table('posts.created'), 'desc')
+            ->take($per_page)
+            ->skip(--$page * $per_page)
+            ->get(array(Base::table('posts.*'),
+                Base::table('users.id as author_id'),
+                Base::table('users.bio as author_bio'),
+                Base::table('users.real_name as author_name')));
+
+        return array($total, $posts);
+    }
+
+    public static function search($term, $page = 1, $per_page = 10)
+    {
+        $query = static::left_join(Base::table('users'), Base::table('users.id'), '=', Base::table('posts.author'))
+            ->where(Base::table('posts.status'), '=', 'published')
+            ->where(Base::table('posts.title'), 'like', '%' . $term . '%')
+            ->or_where(Base::table('posts.html'), 'like', '%' . $term . '%');
+
+        $total = $query->count();
+
+        $posts = $query->take($per_page)
+            ->skip(--$page * $per_page)
+            ->get(array(Base::table('posts.*'),
+                Base::table('users.id as author_id'),
+                Base::table('users.bio as author_bio'),
+                Base::table('users.real_name as author_name')));
+
+        foreach ($posts as $key => $post) {
+            if ($post->data['status'] !== 'published') {
+                unset($posts[$key]);
+            }
+        }
+        if (count($posts) < 1) {
+            $total = 0;
+        }
+
+        return array($total, $posts);
+    }
+
+    public static function perPage()
+    {
+        return (Config::meta('show_all_posts') ? self::count() + 1 : Config::meta('posts_per_page'));
+    }
+
+	public static function tagged($term, $page = 1, $per_page = 10) {
+		    $query = static::left_join(Base::table('users'), Base::table('users.id'), '=', Base::table('posts.author'))
+			->left_join(Base::table('post_meta'), Base::table('post_meta.post'), '=', Base::table('posts.id'))
+			->join(Base::table('extend'), Base::table('extend.id'), '=', Base::table('post_meta.extend'))
+			->where(Base::table('posts.status'), '=', 'published')
+			->where(Base::table('extend.key'), '=', 'tags')
+			->where(Base::table('post_meta.data'), 'like', '{"text":"%'.$term.'%}');
+
+		    $total = $query->count();
+
+		    $posts = $query->sort(Base::table('posts.created'), 'desc')
 			->take($per_page)
 			->skip(--$page * $per_page)
 			->get(array(Base::table('posts.*'),
-				Base::table('users.id as author_id'),
-				Base::table('users.bio as author_bio'),
-				Base::table('users.real_name as author_name')));
+			    Base::table('users.id as author_id'),
+			    Base::table('users.bio as author_bio'),
+			    Base::table('users.real_name as author_name')));
 
-		return array($total, $posts);
-	}
-
-	public static function search($term, $page = 1, $per_page = 10) {
-		$query = static::left_join(Base::table('users'), Base::table('users.id'), '=', Base::table('posts.author'))
-			->where(Base::table('posts.status'), '=', 'published')
-			->where(Base::table('posts.title'), 'like', '%' . $term . '%')
-			->or_where(Base::table('posts.html'), 'like', '%' . $term . '%');
-
-		$total = $query->count();
-
-		$posts = $query->take($per_page)
-			->skip(--$page * $per_page)
-			->get(array(Base::table('posts.*'),
-				Base::table('users.id as author_id'),
-				Base::table('users.bio as author_bio'),
-				Base::table('users.real_name as author_name')));
-
-		foreach ($posts as $key => $post) {
-			if ($post->data['status'] !== 'published') {
-				unset($posts[$key]);
-			}
+		    return array($total, $posts);
 		}
-		if (count($posts) < 1) {
-			$total = 0;
-		}
-
-		return array($total, $posts);
-	}
-
-	public static function perPage() {
-		return (Config::meta('show_all_posts') ? self::count() + 1 : Config::meta('posts_per_page'));
-	}
-
-	public static function tagged($term, $page = 1, $per_page = 10) {
-	    $query = static::left_join(Base::table('users'), Base::table('users.id'), '=', Base::table('posts.author'))
-		->left_join(Base::table('post_meta'), Base::table('post_meta.post'), '=', Base::table('posts.id'))
-		->join(Base::table('extend'), Base::table('extend.id'), '=', Base::table('post_meta.extend'))
-		->where(Base::table('posts.status'), '=', 'published')
-		->where(Base::table('extend.key'), '=', 'tags')
-		->where(Base::table('post_meta.data'), 'like', '{"text":"%'.$term.'%}');
-
-	    $total = $query->count();
-
-	    $posts = $query->sort(Base::table('posts.created'), 'desc')
-		->take($per_page)
-		->skip(--$page * $per_page)
-		->get(array(Base::table('posts.*'),
-		    Base::table('users.id as author_id'),
-		    Base::table('users.bio as author_bio'),
-		    Base::table('users.real_name as author_name')));
-
-	    return array($total, $posts);
-	}
-
 }
